7a5b1b1678debc4cbdfef28564fbd07fd1dee3d5
SalvLav <richardjthies95@gmail.com>
2022-12-03 15:02:26 -0600

    vue app creations, etc

---
+++ hello-vue/vue.config.js
@@ -0,0 +1,6 @@
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  devServer: {
+    proxy: 'http://127.0.0.1:3000'//proxy means to send msgs via vue developer server, which will then be sent to express server @ address
+  }
+}
